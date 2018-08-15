46c60a27ceaf2c0273ac2165376104f063afa3bd
rklar3 <ravan_klar@hotmail.com>
2018-08-15 16:44:17 -0700

    user profile components

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
-
-    private
+    
+    private 
 
     def sign_up_params
         params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
